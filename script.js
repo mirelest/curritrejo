bd01d2bc715f2344ccace10ee28800976e957822
mirelest <kiwijesustm@gmail.com>
2024-08-16 14:49:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    window.location.href = "mailto:email@ejemplo.com?subject=Contacto%20Profesional&body=Hola%20Nombre%20Apellido,%0A%0AEstoy%20interesado%20en%20contactarte%20por%20motivos%20profesionales.%0A%0A¡Gracias!";
+});
